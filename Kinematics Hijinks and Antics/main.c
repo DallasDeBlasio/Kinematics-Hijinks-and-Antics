3e428d44622606378a24ab39ee1ca004c3292c92
Dallas <dallasdeblasio@icloud.com>
2024-02-17 20:33:32 -0800

    Dallas changes to user input and menu First ever stage commit A little scary

--- Kinematics Hijinks and Antics/main.c
+++ Kinematics Hijinks and Antics/main.c
@@ -6,30 +6,9 @@
 int main(void)
 {
 
-	//menu navigation
-	int MainMenuExitCode = 3;
-	int MainMenuNav = 0;
+	MainMenu();
 
-
-
-	int testing = 1;
-	if (testing != 1)
-	{
-		do//main menu loop begin
-		{
-			printf("Welcome to the Kinematics Calculator\n");
-			printf("If you would like to enter our calculator, please enter 1\n");
-			//printf("If you would like to try and find a plant via questions, please enter 2\n");
-			printf("If you would like to exit the program, please enter 2\n");
-			//printf("\n");
-			//printf("\n");
-			
-
-
-
-		} while (MainMenuNav == MainMenuExitCode);//main menu loop end
-	}
-
+	/*
 	OneDVariables variables;
 	variables.hasVariable[veloctiy] = 0;
 	variables.variableValue[veloctiy] = 0;
@@ -53,6 +32,7 @@ int main(void)
 	
 
 	printf("%lf", findingVelocity(variables));
+	*/
 
 	return 14;
 }
